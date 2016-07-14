b48df4926ccbfb6692adbc9125759240b17b26c4
Dane Jordan <dane.e.jordan@gmail.com>
2016-07-13 20:13:49 -0700

    feat: implement fblogin with react

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./public/App.jsx",
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
