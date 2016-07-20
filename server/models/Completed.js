634dd6bece5628d2d9802f735e19f1e7ecd71df3
Trevor Healy <trevorwhealy@gmail.com>
2016-07-20 14:19:47 -0700

    chore: remove unnecessary files from completed.js

--- server/models/Completed.js
+++ server/models/Completed.js
@@ -1,9 +1,5 @@
 var db = require('./sequelize.js');
-var Sequelize = require('sequelize');
-var User = require('./User');
-var Task = require('./Task');
 
 var Completed = db.define('completed', {});
 
 module.exports = Completed;
-
