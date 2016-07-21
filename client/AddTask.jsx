e75df82889c8c95ff9fdb3058eeadf7a65cdfe93
donovanCarreno <donovan_carreno@yahoo.com>
2016-07-20 23:25:03 -0500

    feature: implement addTask

---
+++ client/AddTask.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {Modal} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+
+class AddTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      taskName: '',
+      taskDueDate: ''
+    };
+  }
+
+  close() {
+    this.setState({
+      showModal: false
+    });
+  }
+
+  open() {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  handleTaskNameChange(e) {
+    this.setState({
+      taskName: e.target.value
+    });
+  }
+
+  handleTaskDueDateChange(e) {
+    this.setState({
+      taskDueDate: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let taskName = this.state.taskName;
+    let dueDate =  this.state.taskDueDate;
+
+    if (!taskName || !dueDate) {
+      this.close();
+      return;
+    }
+
+    this.props.onAddNewTask(taskName, dueDate);
+    this.setState({
+      taskName: '',
+      taskDueDate: ''
+    });
+    this.close();
+  }
+
+  render() {
+    return(
+      <div onClick={this.open.bind(this)}>
+        <img src="http://bit.ly/29UZrXq"/>
+        <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form method="POST" onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" name="newTask" placeholder="add task" onChange={this.handleTaskNameChange.bind(this)}/>
+          <input type="date" name="dueDate" onChange={this.handleTaskDueDateChange.bind(this)}/>
+          <input type="submit" value="Add Task"/>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.close.bind(this)}>Cancel</Button>
+        </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default AddTask;
+// <Button bsStyle="primary" type="submit" onClick={this.postOrSaveFunction}>Add</Button>
--- client/App.jsx
+++ client/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router'
 import Login from './Login.jsx'
 import TaskView from './TaskView.jsx'
+import AddTask from './AddTask.jsx'
 
 class App extends React.Component {
   constructor() {
@@ -17,6 +18,15 @@ class App extends React.Component {
     };
   }
 
+  handleAddNewTask(taskName, dueDate) {
+    // this function will handle
+    // posting new task to db &
+    // add to pending tasks
+    // how to implement???
+    console.log('taskName:', taskName);
+    console.log('dueDate:', dueDate);
+  }
+
   render() {
     return (
       <div>
@@ -24,6 +34,9 @@ class App extends React.Component {
           <ul>
             <li><Link to="/login">Login</Link></li>
           </ul>
+        </div>
+        <div>
+          <AddTask onAddNewTask={this.handleAddNewTask.bind(this)}/>
         </div>
         <div>
           <TaskView tasks={this.state.allTasks}/>
