4c7ba8e7302c84b04f201522245ad1979ca91d73
Trevor Healy <trevorwhealy@gmail.com>
2016-07-21 12:23:38 -0700

    feat: app homepage template

--- client/App.jsx
+++ client/App.jsx
@@ -2,22 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router'
 import Login from './Login.jsx'
-import TaskView from './TaskView.jsx'
 
 import Task from './Task.jsx';
 import Completed from './Completed.jsx';
 
 
-import { Panel } from 'react-bootstrap';
-import { Accordion } from 'react-bootstrap';
-
-
+/* Test Data inserted here */
 var data = require('./fakeData');
 // Somehow determines the 5 or fewer most urgent tasks to be complete
-// For test purposes, I will only show 2 as being urgent.
 var urgentTasks = data.allTasks;//.slice(0, 2);
 var completedTasks = data.allCompletedTasks;
 
+
+// Necessary for Material-UI
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Necessary for simple Mobile/Web click functionality on components
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
+// Components we have built
+import Navbar from './Navbar.jsx';
+import Circle from './Circle.jsx';
+import Comp from './Comp.jsx';
+
+
+//import Avatar from 'material-ui/Avatar';
+//import Chip from 'material-ui/Chip';
+//import FontIcon from 'material-ui/FontIcon';
+
+//import AppBar from 'material-ui/AppBar';
+
+let test = () => {
+  console.log('test click worked');
+}
 
 class App extends React.Component {
   constructor() {
@@ -31,55 +50,52 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid">
+      <MuiThemeProvider>
         <div>
-          Create task
-        </div>
-        <div className="row center-block">
-          <div className="col-xs-9">
-            {this.state.urgentTasks.map((urgentTask, i) => {
-              var urgentTaskName = urgentTask.name;
-              var urgentTaskDueBy = urgentTask.dueBy;
+          <Navbar />
 
-              return (
-                <Task name={urgentTaskName} dueBy={urgentTaskDueBy} key={i}/>
-              );
-            })}
+          <div className="row">
+            <div className="col-xs-10">
+              {/* Create the urgentTask bubbles */}
+              {this.state.urgentTasks.map((urgentTask, i) => {
+                let urgentTaskName = urgentTask.name;
+                let urgentTaskDueBy = urgentTask.dueBy.toString();
+                console.log(urgentTaskDueBy);
+                return (
+                  <div className="col-xs-3">
+                    <Circle name={urgentTaskName} dueBy={urgentTaskDueBy} key={i}/>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="col-xs-2">
+              <div className="showAll" onTouchTap={test}>
+              </div>
+            </div>
           </div>
-          <div className="col-xs-3 circle all">
-            all
-          </div>
+
+          {/* Create the completedTasks cards */}
+          {this.state.completedTasks.map((completedTask, i) => {
+            let completedTaskName = completedTask.userId;
+            let completedTaskUser = completedTask.taskId;
+            return (
+              <Comp task={completedTaskName} user={completedTaskUser} key={i}/>
+            );
+          })}
         </div>
-
-
-        <div className="row center-block">
-          <Accordion>
-            {this.state.completedTasks.map((completedTask, i) => {
-              var completedTaskUserID = completedTask.userId;
-              var completedTaskTaskID = completedTask.taskId;
-              return (
-                <Panel header={completedTaskUserID} bsStyle="danger" eventKey={i} >
-                  go head tell me im wrong
-                </Panel>
-              );
-            })}
-          </Accordion>
-        </div>
-
-        <Accordion>
-          <Panel header="sucka" bsStyle="info" eventKey="1">fight me mofo</Panel>
-          <Panel header="panel2" bsStyle="info" eventKey="2">fight me mofo2</Panel>
-          <Panel header="panel2" bsStyle="info" eventKey="3">fight me mofo2</Panel>
-          <Panel header="panel2" bsStyle="info" eventKey="4">fight me mofo2</Panel>
-        </Accordion>
-
-      </div>
+      </MuiThemeProvider>
     );
   }
-
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
+
+// Removed Routes for the sake of building the single App page.
+// TODO: reconnect the app using routers.
+// Curious to see how authentication plays into the current route scheme
+// For instance, we take the user to the main view on load..
+// Whereas I think we should be taking them to login/signup
+// And later, redirect them to App View if their log in was successful.
 
 // ReactDOM.render((
 //   <Router>
