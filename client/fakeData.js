1706f965dbf57db7670bd4276f57657546e23098
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-07-21 18:26:39 -0700

    test: Add basic tests and update fakeData.js

--- client/fakeData.js
+++ client/fakeData.js
@@ -1,24 +1,33 @@
+let hours = n => 1000*60*60*n;
+let days = n => hours(n) * 24;
+
+
 module.exports = {
   allTasks: [
-    {
+    { // urgent
       name: 'walk the dogs',
-      dueBy: new Date()
+      dueBy: new Date(Date.now() + hours(2)),
+      interval: hours(8)
     },
-    {
+    { // urgent
       name: 'clean the dishes',
-      dueBy: new Date()
+      dueBy: new Date(Date.now() + hours(23)),
+      interval: days(2)
     },
     {
       name: 'take out trash',
-      dueBy: new Date()
+      dueBy: new Date(Date.now() + days(4)),
+      interval: days(7)
     },
-    {
+    { // overdue
       name: 'wash the car',
-      dueBy: new Date()
+      dueBy: new Date(Date.now() - days(1)),
+      interval: days(14)
     },
     {
       name: 'pay bills',
-      dueBy: new Date()
+      dueBy: new Date(Date.now() + days(25)),
+      interval: days(31)
     }
   ],
   allCompletedTasks: [
---
+++ spec/urgency.service.spec.js
@@ -0,0 +1,27 @@
+let expect = require('chai').expect;
+let urgency = require('../client/urgency.service');
+let fake = require ('../client/fakeData.js');
+
+describe('urgency.service', function() {
+  it('should accept an empty task list', function() {
+    let tasks = urgency.prioritizeTasks([]);
+    expect(tasks.recent).to.deep.equal([]);
+    expect(tasks.urgent).to.deep.equal([]);
+    expect(tasks.overdue).to.deep.equal([]);
+  });
+  it('should return recently completed tasks', function() {
+    let tasks = urgency.prioritizeTasks(fake.allTasks);
+    let recentTaskNames = tasks.recent.map(t => t.name);
+    expect(recentTaskNames).to.deep.equal(['take out trash', 'pay bills']);
+  });
+  it('should return urgent tasks', function() {
+    let tasks = urgency.prioritizeTasks(fake.allTasks);
+    let urgentTaskNames = tasks.urgent.map(t => t.name);
+    expect(urgentTaskNames).to.deep.equal(['walk the dogs', 'clean the dishes']);
+  });
+  it('should return overdue tasks', function() {
+    let tasks = urgency.prioritizeTasks(fake.allTasks);
+    let overdueTaskNames = tasks.overdue.map(t => t.name);
+    expect(overdueTaskNames).to.deep.equal(['wash the car']);
+  });
+});
