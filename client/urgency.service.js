add5fa79573b8ca171d5b5f4909bd7edb5597a75
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-07-21 18:27:20 -0700

    feat: Implement urgency.service

---
+++ client/urgency.service.js
@@ -0,0 +1,34 @@
+module.exports = {
+  prioritizeTasks
+}
+
+/**
+ * @typedef PrioritizedTasks
+ * @type {object}
+ * @property {Task[]} recent An array of recently completed tasks
+ * @property {Task[]} urgent An array of urgent tasks
+ * @property {Task[]} overdue An array of overdue tasks
+ */
+
+/**
+ * Prioritizes tasks according to their due date and interval length
+ * @param  {object[]} allTasks      An array of all tasks
+ * @return {PrioritizedTasks}       An object grouping tasks by priority
+ */
+function prioritizeTasks(allTasks) {
+  let tasks = { recent: [], urgent: [], overdue: [] };
+  let now = Date.now();
+
+  allTasks.forEach(t => {
+    let timeLeft = t.dueBy - now;
+    if (timeLeft < 0) {
+      return tasks.overdue.push(t);
+    } else if (timeLeft < t.interval / 2) {
+      return tasks.urgent.push(t);
+    } else {
+      return tasks.recent.push(t);
+    }
+  });
+
+  return tasks;
+}
