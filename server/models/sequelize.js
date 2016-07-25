b76ca3e4ef65b6b44c49ab8ae77b51dfb4b5027f
Trevor Healy <trevorwhealy@gmail.com>
2016-07-25 16:42:02 -0700

    chore: add heroku config

--- server/models/sequelize.js
+++ server/models/sequelize.js
@@ -9,9 +9,18 @@ require('pg').types.setTypeParser(1114, function(stringValue) {
   // e.g., UTC offset. Use any offset that you would like.
 });
 
-var db = new Sequelize('rumi', 'rumi', '', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+if (process.env.DATABASE_URL) {
+  var db = new Sequelize(process.env.DATABASE_URL, {
+    host: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    port: 5432
+  });
+
+} else {
+  var db = new Sequelize('rumi', 'rumi', '', {
+    host: 'localhost',
+    dialect: 'postgres',
+  });
+}
 
 module.exports = db;
