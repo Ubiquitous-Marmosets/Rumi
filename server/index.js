0ca3068cc4276170a669611f617348597b745d2d
Dane Jordan <dane.e.jordan@gmail.com>
2016-07-25 17:32:26 -0700

    bug:dynamic port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ let server = require('./server');
 let db = require('./models/config');
 
 db.sync().then(() => {
-  server.listen(3000, () => {
-    console.log('Listening on port 3000');
+  server.listen(process.env.PORT || 3000, () => {
+    
+    console.log('Listening....');
   });
 });
